000867c486ba10a8429ce525340e986ebccb7a21
Amanda Sumner <amandamsumner@me.com>
2021-05-31 20:43:51 -0400

    GitIgnore

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "181615243869033",
+    clientSecret: "0ff3ff051e24e54492b8c557c6044f3d",
+  },
 };
